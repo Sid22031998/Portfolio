b845e539b66ae81887c18a2dde96e01e5c1dd5a4
Siddharth <siddharthrosesforyou@gmail.com>
2019-03-23 19:57:55 +0530

    make it better

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express=require("express");
 var app=express();
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 var bodyParser=require("body-parser");
 app.use(express.static(__dirname + "/public"));
 
 
 app.get("/", function(req, res){
-   res.render("index"); 
+   res.render("index.html"); 
 });
 
 
