5fff6aa7f0e519a3a781ae0468d753e631f1fcc6
Siddharth <siddharthrosesforyou@gmail.com>
2019-03-23 19:48:49 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,15 @@
+var express=require("express");
+var app=express();
+app.set("view engine", "ejs");
+var bodyParser=require("body-parser");
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/", function(req, res){
+   res.render("index"); 
+});
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Server started...');
+});
